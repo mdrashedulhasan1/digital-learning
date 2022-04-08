959a5637be564f6f0587f8f92c438bfebc7afaaf
Rashedul Hasan <Rashedul Hasan>
2022-04-08 22:40:56 +0600

    Half part of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Pages/Home/Home/Home';
+import PopularCourses from './Pages/Home/PopularCourses/PopularCourses';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/popular-courses">
+            <PopularCourses></PopularCourses>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
